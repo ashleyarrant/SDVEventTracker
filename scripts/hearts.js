a2a0b1914f0ed1ae1a4e59f24b999f75f44b3bf2
Ashley <32113554+ashleyarrant@users.noreply.github.com>
2022-10-28 12:39:15 -0700

    update

--- scripts/hearts.js
+++ scripts/hearts.js
@@ -1,7 +1,11 @@
-function oneheart() {
-	alert('one heart');
+function one(name) {
+	$("#currently-"+name).find("img").attr("src","empty.png");
+	$("#currently-"+name).find("img").eq(0).attr("src","filled.png");
 }
 
-function twohearts() {
-	alert("two hearts");
+function two(name) {
+	$("#currently-"+name).find("img").attr("src","empty.png");
+	for(let i=0;i<2; i++){
+		$("#currently-"+name).find("img").eq(i).attr("src","filled.png");
+	}
 }
