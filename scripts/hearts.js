ca4439f55992c746fe07a96e58ac7503cfd1dec5
Ashley <32113554+ashleyarrant@users.noreply.github.com>
2022-10-28 13:28:42 -0700

    update

--- scripts/hearts.js
+++ scripts/hearts.js
@@ -1,6 +1,7 @@
 function one(name) {
 	$("#currently-"+name).find("img").attr("src","empty.png");
 	$("#currently-"+name).find("img").eq(0).attr("src","filled.png");
+	$("#currently-"+name).attr("data-value","1");
 }
 
 function two(name) {
@@ -8,4 +9,101 @@ function two(name) {
 	for(let i=0;i<2; i++){
 		$("#currently-"+name).find("img").eq(i).attr("src","filled.png");
 	}
+	$("#currently-"+name).attr("data-value","2");
+}
+
+function three(name) {
+	$("#currently-"+name).find("img").attr("src","empty.png");
+	for(let i=0;i<3; i++){
+		$("#currently-"+name).find("img").eq(i).attr("src","filled.png");
+	}
+	$("#currently-"+name).attr("data-value","3");
+}
+
+function four(name) {
+	$("#currently-"+name).find("img").attr("src","empty.png");
+	for(let i=0;i<4; i++){
+		$("#currently-"+name).find("img").eq(i).attr("src","filled.png");
+	}
+	$("#currently-"+name).attr("data-value","4");
+}
+
+function five(name) {
+	$("#currently-"+name).find("img").attr("src","empty.png");
+	for(let i=0;i<5; i++){
+		$("#currently-"+name).find("img").eq(i).attr("src","filled.png");
+	}
+	$("#currently-"+name).attr("data-value","5");
+}
+
+function six(name) {
+	$("#currently-"+name).find("img").attr("src","empty.png");
+	for(let i=0;i<6; i++){
+		$("#currently-"+name).find("img").eq(i).attr("src","filled.png");
+	}
+	$("#currently-"+name).attr("data-value","6");
+}
+
+function seven(name) {
+	$("#currently-"+name).find("img").attr("src","empty.png");
+	for(let i=0;i<7; i++){
+		$("#currently-"+name).find("img").eq(i).attr("src","filled.png");
+	}
+	$("#currently-"+name).attr("data-value","7");
+}
+
+function eight(name) {
+	$("#currently-"+name).find("img").attr("src","empty.png");
+	for(let i=0;i<8; i++){
+		$("#currently-"+name).find("img").eq(i).attr("src","filled.png");
+	}
+	$("#currently-"+name).attr("data-value","8");
+}
+
+function nine(name) {
+	$("#currently-"+name).find("img").attr("src","empty.png");
+	for(let i=0;i<9; i++){
+		$("#currently-"+name).find("img").eq(i).attr("src","filled.png");
+	}
+	$("#currently-"+name).attr("data-value","9");
+}
+
+function ten(name) {
+	$("#currently-"+name).find("img").attr("src","empty.png");
+	for(let i=0;i<10; i++){
+		$("#currently-"+name).find("img").eq(i).attr("src","filled.png");
+	}
+	$("#currently-"+name).attr("data-value","10");
+}
+
+function eleven(name) {
+	$("#currently-"+name).find("img").attr("src","empty.png");
+	for(let i=0;i<11; i++){
+		$("#currently-"+name).find("img").eq(i).attr("src","filled.png");
+	}
+	$("#currently-"+name).attr("data-value","11");
+}
+
+function twelve(name) {
+	$("#currently-"+name).find("img").attr("src","empty.png");
+	for(let i=0;i<12; i++){
+		$("#currently-"+name).find("img").eq(i).attr("src","filled.png");
+	}
+	$("#currently-"+name).attr("data-value","12");
+}
+
+function thirteen(name) {
+	$("#currently-"+name).find("img").attr("src","empty.png");
+	for(let i=0;i<13; i++){
+		$("#currently-"+name).find("img").eq(i).attr("src","filled.png");
+	}
+	$("#currently-"+name).attr("data-value","13");
+}
+
+function fourteen(name) {
+	$("#currently-"+name).find("img").attr("src","empty.png");
+	for(let i=0;i<14; i++){
+		$("#currently-"+name).find("img").eq(i).attr("src","filled.png");
+	}
+	$("#currently-"+name).attr("data-value","14");
 }
