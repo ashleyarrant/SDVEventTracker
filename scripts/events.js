bcb866b3d0b6b6a422ab7d9ba779ea942813b24f
Ashley <32113554+ashleyarrant@users.noreply.github.com>
2022-10-28 17:20:42 -0700

    update

--- scripts/events.js
+++ scripts/events.js
@@ -1,11 +1,18 @@
-function seenEvent(x){
+function setSeenEvent(x){
 	let event = $(x).attr("id").split("-")[0];
 	let name = $(x).attr("id").split("-")[1];
 	let seen = x.checked;
 	let cname="seen"+event+name;
 	if (seen){
 		setCookie(cname,"true",100);
-	} else {
-		setCookie(cname,"false",100);
 	}
+}
+
+function checkSeenEvent(x) {
+	let event = $(x).attr("id").split("-")[0];
+	let name = $(x).attr("id").split("-")[1];
+	let cname="seen"+event+name;
+
+	alert("hi");
+
 }
