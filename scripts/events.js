622a1551d9574a4e0e9f8830444502da11ecdc4b
Ashley <32113554+ashleyarrant@users.noreply.github.com>
2022-10-30 23:42:58 -0700

    functionality update

--- scripts/events.js
+++ scripts/events.js
@@ -12,10 +12,12 @@ function setSeenEvent(x){
 		let newCookie = cookie + eventName;
 		setCookie(cname,newCookie,100);
 		$("label[for='"+$(x).attr("id")+"']").addClass("selected");
+		$("#box-"+eventName+"-"+name).removeClass("bg-warning");
 	} else {
 		let newCookie = cookie.replace(eventName,"");
 		setCookie(cname,newCookie,100);
 		$("label[for='"+$(x).attr("id")+"']").removeClass("selected");
+		$("#box-"+eventName+"-"+name).addClass("bg-warning");
 	}
 }
 
@@ -28,72 +30,21 @@ function loadSeen(eventName, name){
 	//if cookie contains event name, check it off
 	if(cookie.includes(eventName)){
 		$("label[for='"+checkbox+"']").addClass("selected");
+		$("#box-"+eventName+"-"+name).removeClass("bg-warning");
 		return "checked";
 	} else {
 		$("label[for='"+checkbox+"']").removeClass("selected");
+		$("#box-"+eventName+"-"+name).addClass("bg-warning");
 		return "";
 	}
 }
 
-// onclick event when the dropdown is changed
-function setRelationship(x) {
-	let name = $(x).attr("id").replace("relationship-","");
-	let status = $(x).val();
-	let cookie = "", newCookie = "";
-	
-	if(status = "single") {
-		cookie = getCookie("dating");
-		cookie.replace(name,"");
+function relationship(name){
+	let hearts = parseInt($("#currently-"+name).attr("data-value"));
 
-		cookie = getCookie("married");
-		cookie.replace(name,"");
-
-		if(getCookie("single").includes(name) == false){
-			cookie = getCookie("single");
-			newCookie = cookie + name;
-
-			setCookie("single",newCookie,100);
-		}
-	} else if(status = "dating") {
-		cookie = getCookie("single");
-		cookie.replace(name,"");
-
-		cookie = getCookie("married");
-		cookie.replace(name,"");
-
-		if(getCookie("dating").includes(name) == false){
-			cookie = getCookie("dating");
-			newCookie = cookie + name;
-
-			setCookie("dating",newCookie,100);
-		}
-	} else if(status = "married"){
-		cookie = getCookie("dating");
-		cookie.replace(name,"");
-
-		cookie = getCookie("single");
-		cookie.replace(name,"");
-
-		if(getCookie("married").includes(name) == false){
-			cookie = getCookie("married");
-			newCookie = cookie + name;
-
-			setCookie("married",newCookie,100);
-		}
-	}
-
-}
-
-function loadRelationship(name){
-	let a = getCookie("single");
-	let b = getCookie("dating");
-	let c = getCookie("married");
-
-	if(a.includes(name)){
-		$("#relationship-"+name).val("single");
-	} else if(b.includes(name)){
-		$("#relationship-"+name).val("dating");
-	} else if(c.includes(name)){
-		$("#relationship-"+name).val("married");
-	}
+	if(hearts>10){
+		return "married";
+	} else if(hearts>8){
+		return "dating";
+	} else return "single";
 }
--- scripts/hearts.js
+++ scripts/hearts.js
@@ -3,6 +3,13 @@ function one(name) {
 	$("#currently-"+name).find("img").eq(0).attr("src","filled.png");
 	$("#currently-"+name).attr("data-value","1");
 	setCookie(name+"Hearts","1",100);
+	let r = $("#relationship-"+name);
+	if(r.length!=0){
+		r.text("(single)");
+	}
+	if($("#oneHeart-"+name).prop("checked") == false){
+		$("#box-oneHeart-"+name).addClass("bg-warning");
+	}
 }
 
 function two(name) {
@@ -12,6 +19,13 @@ function two(name) {
 	}
 	$("#currently-"+name).attr("data-value","2");
 	setCookie(name+"Hearts","2",100);
+	let r = $("#relationship-"+name);
+	if(r.length!=0){
+		r.text("(single)");
+	}
+	if($("#twoHearts-"+name).prop("checked") == false){
+		$("#box-twoHearts-"+name).addClass("bg-warning");
+	}
 }
 
 function three(name) {
@@ -21,6 +35,13 @@ function three(name) {
 	}
 	$("#currently-"+name).attr("data-value","3");
 	setCookie(name+"Hearts","3",100);
+	let r = $("#relationship-"+name);
+	if(r.length!=0){
+		r.text("(single)");
+	}
+	if($("#threeHearts-"+name).prop("checked") == false){
+		$("#box-threeHearts-"+name).addClass("bg-warning");
+	}
 }
 
 function four(name) {
@@ -30,6 +51,13 @@ function four(name) {
 	}
 	$("#currently-"+name).attr("data-value","4");
 	setCookie(name+"Hearts","4",100);
+	let r = $("#relationship-"+name);
+	if(r.length!=0){
+		r.text("(single)");
+	}
+	if($("#fourHearts-"+name).prop("checked") == false){
+		$("#box-fourHearts-"+name).addClass("bg-warning");
+	}
 }
 
 function five(name) {
@@ -39,6 +67,13 @@ function five(name) {
 	}
 	$("#currently-"+name).attr("data-value","5");
 	setCookie(name+"Hearts","5",100);
+	let r = $("#relationship-"+name);
+	if(r.length!=0){
+		r.text("(single)");
+	}
+	if($("#fiveHearts-"+name).prop("checked") == false){
+		$("#box-fiveHearts-"+name).addClass("bg-warning");
+	}
 }
 
 function six(name) {
@@ -48,6 +83,13 @@ function six(name) {
 	}
 	$("#currently-"+name).attr("data-value","6");
 	setCookie(name+"Hearts","6",100);
+	let r = $("#relationship-"+name);
+	if(r.length!=0){
+		r.text("(single)");
+	}
+	if($("#sixHearts-"+name).prop("checked") == false){
+		$("#box-sixHearts-"+name).addClass("bg-warning");
+	}
 }
 
 function seven(name) {
@@ -57,6 +99,13 @@ function seven(name) {
 	}
 	$("#currently-"+name).attr("data-value","7");
 	setCookie(name+"Hearts","7",100);
+	let r = $("#relationship-"+name);
+	if(r.length!=0){
+		r.text("(single)");
+	}
+	if($("#sevenHearts-"+name).prop("checked") == false){
+		$("#box-sevenHearts-"+name).addClass("bg-warning");
+	}
 }
 
 function eight(name) {
@@ -66,6 +115,13 @@ function eight(name) {
 	}
 	$("#currently-"+name).attr("data-value","8");
 	setCookie(name+"Hearts","8",100);
+	let r = $("#relationship-"+name);
+	if(r.length!=0){
+		r.text("(dating)");
+	}
+	if($("#eightHearts-"+name).prop("checked") == false){
+		$("#box-eightHearts-"+name).addClass("bg-warning");
+	}
 }
 
 function nine(name) {
@@ -75,6 +131,13 @@ function nine(name) {
 	}
 	$("#currently-"+name).attr("data-value","9");
 	setCookie(name+"Hearts","9",100);
+	let r = $("#relationship-"+name);
+	if(r.length!=0){
+		r.text("(dating)");
+	}
+	if($("#nineHearts-"+name).prop("checked") == false){
+		$("#box-nineHearts-"+name).addClass("bg-warning");
+	}
 }
 
 function ten(name) {
@@ -84,6 +147,13 @@ function ten(name) {
 	}
 	$("#currently-"+name).attr("data-value","10");
 	setCookie(name+"Hearts","10",100);
+	let r = $("#relationship-"+name);
+	if(r.length!=0){
+		r.text("(dating)");
+	}
+	if($("#tenHearts-"+name).prop("checked") == false){
+		$("#box-tenHearts-"+name).addClass("bg-warning");
+	}
 }
 
 function eleven(name) {
@@ -93,6 +163,13 @@ function eleven(name) {
 	}
 	$("#currently-"+name).attr("data-value","11");
 	setCookie(name+"Hearts","11",100);
+	let r = $("#relationship-"+name);
+	if(r.length!=0){
+		r.text("(spouse)");
+	}
+	if($("#elevenHearts-"+name).prop("checked") == false){
+		$("#box-elevenHearts-"+name).addClass("bg-warning");
+	}
 }
 
 function twelve(name) {
@@ -102,6 +179,13 @@ function twelve(name) {
 	}
 	$("#currently-"+name).attr("data-value","12");
 	setCookie(name+"Hearts","12",100);
+	let r = $("#relationship-"+name);
+	if(r.length!=0){
+		r.text("(spouse)");
+	}
+	if($("#twelveHearts-"+name).prop("checked") == false){
+		$("#box-twelveHearts-"+name).addClass("bg-warning");
+	}
 }
 
 function thirteen(name) {
@@ -111,6 +195,13 @@ function thirteen(name) {
 	}
 	$("#currently-"+name).attr("data-value","13");
 	setCookie(name+"Hearts","13",100);
+	let r = $("#relationship-"+name);
+	if(r.length!=0){
+		r.text("(spouse)");
+	}
+	if($("#thirteenHearts-"+name).prop("checked") == false){
+		$("#box-thirteenHearts-"+name).addClass("bg-warning");
+	}
 }
 
 function fourteen(name) {
@@ -120,4 +211,11 @@ function fourteen(name) {
 	}
 	$("#currently-"+name).attr("data-value","14");
 	setCookie(name+"Hearts","14",100);
+	let r = $("#relationship-"+name);
+	if(r.length!=0){
+		r.text("(spouse)");
+	}
+	if($("#fourteenHearts-"+name).prop("checked") == false){
+		$("#box-fourteenHearts-"+name).addClass("bg-warning");
+	}
 }
